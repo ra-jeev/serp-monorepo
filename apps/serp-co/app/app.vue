57f039a29cd60d595fd6483d6c6690661dfafe86
Rajeev R Sharma <i.rarsh@gmail.com>
2025-09-09 19:49:14 +0530

    chore: add serp/ui package to serp-co app

--- apps/serp-co/app/app.vue
+++ apps/serp-co/app/app.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div>
+      <NuxtRouteAnnouncer />
+      <h1 class="text-3xl font-bold p-4">
+        Serp Co
+      </h1>
+      <HelloWorld />
+    </div>
+  </UApp>
 </template>
