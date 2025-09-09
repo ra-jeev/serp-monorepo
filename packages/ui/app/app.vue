29ed442e58c41504091ccf05cd9af93e53fa03b7
Rajeev R Sharma <i.rarsh@gmail.com>
2025-09-09 19:46:08 +0530

    feat: add ui package with nuxt/ui module

---
+++ packages/ui/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <UApp>
+    <HelloWorld />
+  </UApp>
+</template>
---
+++ packages/ui/nuxt.config.ts
@@ -0,0 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+export default defineNuxtConfig({
+  modules: ['@nuxt/ui'],
+  devtools: { enabled: true },
+  css: [join(currentDir, './app/assets/css/main.css')],
+});
