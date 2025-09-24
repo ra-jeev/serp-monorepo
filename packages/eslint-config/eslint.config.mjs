903e7022e3eff8d6a6c0b6f9451cab6ae8df3ad2
Rajeev R Sharma <i.rarsh@gmail.com>
2025-09-24 18:59:36 +0530

    chore: minor eslint rules update

--- packages/eslint-config/eslint.config.mjs
+++ packages/eslint-config/eslint.config.mjs
@@ -1,17 +1,21 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config';
 
-export default createConfigForNuxt({
-  features: {
-    typescript: {
-      strict: true,
-    },
-    stylistic: {
-      braceStyle: '1tbs',
-      arrowParens: true,
-      semi: true,
+export default createConfigForNuxt(
+  {
+    features: {
+      typescript: {
+        strict: true,
+      },
+      stylistic: {
+        braceStyle: '1tbs',
+        arrowParens: true,
+        semi: true,
+      },
     },
   },
-}, { ignores: ['templates'] }).overrideRules({
+  { ignores: ['templates'] },
+).overrideRules({
+  'vue/singleline-html-element-content-newline': 'off',
   'vue/multi-word-component-names': 'off',
   'vue/max-attributes-per-line': ['error', { singleline: 5 }],
   '@typescript-eslint/ban-types': 'off',
