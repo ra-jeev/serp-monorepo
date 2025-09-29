b28ff233a16e9785b3eb48ed142afe4e93a316bd
Rajeev R Sharma <i.rarsh@gmail.com>
2025-09-29 18:33:44 +0530

    chore: configure cloudflare module preset for nitro

--- apps/serp-co/nuxt.config.ts
+++ apps/serp-co/nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@serp/ui'],
+  modules: ['nitro-cloudflare-dev'],
   devtools: { enabled: true },
   css: ['~/assets/css/typography.css'],
   compatibilityDate: '2025-07-15',
+  nitro: {
+    preset: 'cloudflare_module',
+    // rollupConfig: {
+    //   external: ['pg-native'],
+    // },
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+    },
+  },
 });
