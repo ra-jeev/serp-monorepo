75e1f294965134279cc295516a5073361be5cc52
Rajeev R Sharma <i.rarsh@gmail.com>
2025-10-30 18:48:33 +0530

    chore: add @nuxtjs/seo module

--- apps/serp-co/nuxt.config.ts
+++ apps/serp-co/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@serp/ui'],
-  modules: ['nitro-cloudflare-dev'],
+  modules: ['nitro-cloudflare-dev', '@nuxtjs/seo'],
   devtools: { enabled: true },
   css: ['~/assets/css/typography.css'],
   compatibilityDate: '2025-07-15',
