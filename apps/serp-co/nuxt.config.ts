e207408f40f29a638b020bf760a94c4e04e278ef
Rajeev R Sharma <i.rarsh@gmail.com>
2025-10-30 18:51:27 +0530

    feat: add sitemaps and other seo stuffs to nuxt config

--- apps/serp-co/nuxt.config.ts
+++ apps/serp-co/nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   modules: ['nitro-cloudflare-dev', '@nuxtjs/seo'],
   devtools: { enabled: true },
   css: ['~/assets/css/typography.css'],
+  site: {
+    url: process.env.NUXT_PUBLIC_SITE_URL || 'https://serp.co',
+    name: process.env.NUXT_PUBLIC_SITE_NAME || 'SERP',
+  },
   compatibilityDate: '2025-07-15',
   nitro: {
     preset: 'cloudflare_module',
@@ -13,6 +17,34 @@ export default defineNuxtConfig({
     cloudflare: {
       deployConfig: true,
       nodeCompat: true,
+    },
+  },
+  ogImage: {
+    enabled: false,
+  },
+  schemaOrg: {
+    identity: 'Organization',
+  },
+  seo: {
+    meta: {
+      themeColor: [
+        { content: '#18181b', media: '(prefers-color-scheme: dark)' },
+        { content: 'white', media: '(prefers-color-scheme: light)' },
+      ],
+    },
+  },
+  sitemap: {
+    sitemapsPathPrefix: '/',
+    sitemaps: {
+      ['sitemap-pages']: {
+        includeAppSources: true,
+      },
+      ['sitemap-products']: {
+        sources: ['/api/__sitemap__/urls/products'],
+      },
+      ['sitemap-posts']: {
+        sources: ['/api/__sitemap__/urls/posts'],
+      },
     },
   },
 });
